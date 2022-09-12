99416b6da49a102ad0e02daf117626da40297aa9
Grebenkin Konstantin <ko.klochkov@gmail.com>
2022-09-12 18:53:21 +0300

    orchid posts

---
+++ app/Orchid/Screens/PostEditScreen.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Orchid\Screens;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Fields\Quill;
+use Orchid\Screen\Fields\Relation;
+use Orchid\Screen\Fields\SimpleMDE;
+use Orchid\Screen\Fields\TextArea;
+use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Alert;
+use Orchid\Support\Facades\Layout;
+
+class PostEditScreen extends Screen
+{
+
+    public $post;
+    /**
+     * Query data.
+     *
+     * @return array
+     */
+    public function query(Post $post): iterable
+    {
+        return [
+            'post' => $post
+        ];
+    }
+
+    /**
+     * Display header name.
+     *
+     * @return string|null
+     */
+    public function name(): ?string
+    {
+        return $this->post->exists ? 'Edit post' : 'Creating a new post';
+    }
+
+    public function description(): ?string
+    {
+        return "Blog posts";
+    }
+
+    /**
+     * Button commands.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
+    public function commandBar(): iterable
+    {
+        return [
+            Button::make('Create post')
+                ->icon('pencil')
+                ->method('createOrUpdate')
+                ->canSee(!$this->post->exists),
+
+            Button::make('Update')
+                ->icon('note')
+                ->method('createOrUpdate')
+                ->canSee($this->post->exists),
+
+            Button::make('Remove')
+                ->icon('trash')
+                ->method('remove')
+                ->canSee($this->post->exists),
+        ];
+    }
+
+    /**
+     * Views.
+     *
+     * @return \Orchid\Screen\Layout[]|string[]
+     */
+    public function layout(): iterable
+    {
+        return [
+            Layout::rows([
+                Input::make('post.title')
+                    ->title('Title')
+                    ->placeholder('Attractive but mysterious title')
+                    ->help('Specify a short descriptive title for this post.'),
+
+                TextArea::make('post.description')
+                    ->title('Description')
+                    ->rows(3)
+                    ->maxlength(200)
+                    ->placeholder('Brief description for preview'),
+
+                Relation::make('post.author')
+                    ->title('Author')
+                    ->fromModel(User::class, 'name'),
+
+                SimpleMDE::make('post.markdown')
+
+            ])
+        ];
+    }
+    /**
+     * @param Post    $post
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function createOrUpdate(Post $post, Request $request)
+    {
+        $post->fill($request->get('post'))->save();
+
+        Alert::info('You have successfully created a post.');
+
+        return redirect()->route('platform.post.list');
+    }
+
+    /**
+     * @param Post $post
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
+     */
+    public function remove(Post $post)
+    {
+        $post->delete();
+
+        Alert::info('You have successfully deleted the post.');
+
+        return redirect()->route('platform.post.list');
+    }
+}
